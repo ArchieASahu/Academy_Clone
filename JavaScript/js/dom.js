c47eee669e399749cf0287e006957e944469db09
Archie Sahu <archie02sahu@gmail.com>
2024-05-09 20:41:26 +0530

    Completed

---
+++ JavaScript/js/dom.js
@@ -0,0 +1,19 @@
+// let h = document.getElementById("js")
+// let h = document.getElementsByTagName("h1")
+// let h = document.getElementsByClassName("heading")
+// console.log(h[1]);
+
+// let h = document.querySelector(".heading")
+// let hs = document.querySelectorAll(".heading")
+// let h = document.querySelector("#js")
+let h = document.querySelector("#js")
+
+// console.dir(h);
+console.log(h.innerHTML);
+// h.innerHTML="Django"
+// h.style.color ='red'
+// h.style.backgroundColor ='yellow'
+// h.style.display='none'
+// console.log(hs);
+// h.classList.add("mystyle")
+h.classList.remove("heading")
